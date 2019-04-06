e250e55208aff6e45d266e8036104f7042b737ac
akshay-shrivastava <aksshayy@gmail.com>
2019-04-06 17:50:38 +0530

    added eslint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.scss'
 import App from './App'
 import { Route } from 'react-router-dom'
 import { ConnectedRouter } from 'connected-react-router'
-// import * as serviceWorker from './serviceWorker'
+import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
 import configureStore, { history } from './store'
 
@@ -21,4 +21,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister()
+serviceWorker.unregister()
