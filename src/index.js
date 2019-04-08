0a36c1b5edbc228a648edad27b437313e691774d
akshay-shrivastava <aksshayy@gmail.com>
2019-04-08 10:34:58 +0530

    installed eslint and fixed minor issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'babel-polyfill'
+import '@babel/polyfill'
 import './index.scss'
 import App from './App'
 import { Route } from 'react-router-dom'
